fc88dee96e432f253e902797e7c488fd895538e7
ethanetexplorer <ethanetexplorer@outlook.com>
2024-06-13 08:51:54 +0800

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Ethan Lim on 13/6/24.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
