e160a9d217dc9d25b9e3591c57a7f2eadfb9394a
ethanetExplorer <ethanetexplorer@outlook.com>
2024-08-16 21:28:08 +0800

    Refactoring
    Moved TimerItemView to separate file
    Added logic in start function of TimerItem to set initial time
    Added press-and-hold function to delete key in timer keyboard
    Moved global control buttons for stopwatches out of navigation bar

---
+++ Timetrack/Stopwatch/Toast.swift
@@ -0,0 +1,7 @@
+//
+//  Toast.swift
+//  Stopwatch
+//
+//  Created by Ethan Lim on 16/8/24.
+//
+
---
+++ Timetrack/Timer/TimerItemView.swift
@@ -0,0 +1,18 @@
+//
+//  TImerItemView.swift
+//  Stopwatch
+//
+//  Created by Ethan Lim on 16/8/24.
+//
+
+import SwiftUI
+
+struct TImerItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TImerItemView()
+}
