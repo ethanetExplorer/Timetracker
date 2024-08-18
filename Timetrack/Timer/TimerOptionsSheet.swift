66d5c8ba2b297499ea45426aaa280ef97596db32
ethanetExplorer <ethanetexplorer@outlook.com>
2024-08-18 21:35:05 +0800

    Added new sheet to present more timer options
    Also cleaned up buttons in TimersView and StopwatchMainView
    Added colorset for text colors

---
+++ Timetrack/Timer/TimerOptionsSheet.swift
@@ -0,0 +1,18 @@
+//
+//  TimerOptionsSheet.swift
+//  Stopwatch
+//
+//  Created by Ethan Lim on 17/8/24.
+//
+
+import SwiftUI
+
+struct TimerOptionsSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerOptionsSheet()
+}
