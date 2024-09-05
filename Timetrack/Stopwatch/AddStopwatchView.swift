c0dcd947dbd2f95710d366f412da4fe90a946a72
ethanetExplorer <ethanetexplorer@outlook.com>
2024-09-05 21:02:00 +0800

    Changed @Query macro to persist 1 single [Stopwatch] array instead of [StopwatchSet

---
+++ Timetrack/Stopwatch/AddStopwatchView.swift
@@ -0,0 +1,18 @@
+//
+//  AddStopwatchView.swift
+//  Stopwatch
+//
+//  Created by Ethan Lim on 5/9/24.
+//
+
+import SwiftUI
+
+struct AddStopwatchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddStopwatchView()
+}
